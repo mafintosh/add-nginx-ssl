f30b10659eb97150ca4d978bd774815540b63a02
Mathias Buus <mathiasbuus@gmail.com>
2017-10-27 09:11:17 +0200

    add support for letsencrypt (#3)

--- index.sh
+++ index.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -e
+
 ALL=false
 HELP=false
 DOMAINS=()
+LETSENCRYPT=false
+[ "$1" == "" ] && HELP=true
+
 # From https://wiki.mozilla.org/Security/Server_Side_TLS#Modern_compatibility
-MODERN_CIHPERS="ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256"
+MODERN_CIPHERS="ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256"
 LEGACY_CIPHERS="ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS"
 UNSAFE_CIPHERS="ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-RSA-DES-CBC3-SHA:ECDHE-ECDSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:DES-CBC3-SHA:HIGH:SEED:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!RSAPSK:!aDH:!aECDH:!EDH-DSS-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA:!SRP"
 CIPHERS="$MODERN_CIPHERS"
@@ -16,6 +21,8 @@ TLS_VERSIONS="$MODERN_TLS"
 
 while true; do
   case "$1" in
+    --letsencrypt)     LETSENCRYPT=true; shift ;;
+    -l)                LETSENCRYPT=true; shift ;;
     --all)             ALL=true; shift ;;
     -a)                ALL=true; shift ;;
     --domain)          DOMAINS+=("$2"); shift; shift ;;
@@ -44,16 +51,52 @@ error () {
   exit 1
 }
 
+check_program () {
+  [ "$(which $1) 2>/dev/null" != "" ] && return 0
+  error "$1 is required"
+}
+
 check_file () {
   [ -f "$1" ] && return 0
   error "$1 does not exist"
 }
 
+renew_cert_timer () {
+  cat << EOF > /etc/systemd/system/certbot.service
+[Unit]
+Description=Lets Encrypt renewal
+
+[Service]
+Type=oneshot
+ExecStart=$(which certbot) renew --quiet --agree-tos
+EOF
+
+  cat << EOF > /etc/systemd/system/certbot.timer
+[Unit]
+Description=Twice daily renewal of Lets Encrypt certificates
+
+[Timer]
+OnCalendar=0/12:00:00
+RandomizedDelaySec=1h
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+
+  systemctl enable certbot.timer
+  systemctl start certbot.timer
+
+  echo "Started certificate renew timer"
+}
+
+
 if $HELP; then
-  cat <<EOF_HELP
+  cat <<EOF
 Usage: add-nginx-ssl [options]
-  --key,            -k  ssl-private-key.key (required)
-  --cert,           -c  ssl-certificate.crt (required)
+  --key,            -k  ssl-private-key.key (required if no --letsencrypt)
+  --cert,           -c  ssl-certificate.crt (required if no --letsencrypt)
+  --letsencrypt     -l  use letsencrypt to issue and auto renew certs
   --dhparam,        -p  dhparam.pem
   --all,            -a  (add ssl to all domains)
   --domain,         -d  example.com
@@ -64,21 +107,67 @@ Usage: add-nginx-ssl [options]
   --legacy-tls          accept all TLS versions
   --unsafe-tls          accept all TLS versions and SSLv3
 
-EOF_HELP
+EOF
   exit 0
 fi
 
+check_program nginx
+
 [ ! -d /etc/nginx/conf.d ] && error "/etc/nginx/conf.d does not exist. Is nginx installed?"
+
+[ "$KEY" == "" ] && ! $LETSENCRYPT && error "--key is required"
+[ "$CERT" == "" ] && ! $LETSENCRYPT && error "--cert is required"
+[ "$DOMAINS" == "" ] && ! $ALL && error "--domain or --all is required"
+$ALL && DOMAINS+=('-')
+
+[ ! -O /etc/nginx/conf.d ] && SUDO_MAYBE=sudo
+
+if $ALL && $LETSENCRYPT; then
+  error "Cannot both have --letsencrypt and --all"
+fi
+
+for DOMAIN in ${DOMAINS[@]}; do
+  DOMAIN_LIST="$DOMAIN_LIST -d $DOMAIN"
+done
+
+if $LETSENCRYPT; then
+  check_program openssl
+  check_program certbot
+  check_program systemctl
+
+  FIRST_DOMAIN="${DOMAINS[0]}"
+  LETSENCRYPT_CERTS="/etc/letsencrypt/live/$FIRST_DOMAIN"
+  $SUDO_MAYBE mkdir -p /var/www/letsencrypt
+
+  if [ ! -f /etc/nginx/conf.d/ssl.conf ] || ! grep '/.well-known/acme-challenge' /etc/nginx/conf.d/ssl.conf >/dev/null 2>/dev/null; then
+    cat <<EOF > /tmp/nginx.ssl.conf
+server {
+  listen 80;
+  server_name _;
+
+  location /.well-known/acme-challenge {
+    root /var/www/letsencrypt;
+  }
+}
+EOF
+    $SUDO_MAYBE mv /tmp/nginx.ssl.conf /etc/nginx/conf.d/ssl.conf
+    $SUDO_MAYBE nginx -s reload
+  fi
+
+  DHPARAM="$LETSENCRYPT_CERTS/dhparam2048.pem"
+  [ ! -f "$DHPARAM" ] && openssl dhparam -outform pem -out "$DHPARAM" 2048
+
+  $SUDO_MAYBE mkdir -p /var/www/letsencrypt
+  $SUDO_MAYBE certbot certonly $DOMAIN_LIST --expand --webroot -n --agree-tos --register-unsafely-without-email --webroot-path /var/www/letsencrypt
+
+  KEY="$LETSENCRYPT_CERTS/privkey.pem"
+  CERT="$LETSENCRYPT_CERTS/fullchain.pem"
+fi
 
 if [ "$DHPARAM" != "" ]; then
   check_file "$DHPARAM"
-  SSL_DHPARAM="ssl_dhparam $(realpath $DHPARAM);";
+  SSL_DHPARAM="ssl_dhparam $(realpath -s $DHPARAM);";
 fi
-
-[ "$KEY" == "" ] && error "--key is required"
-[ "$CERT" == "" ] && error "--cert is required"
-[ "$DOMAINS" == "" ] && ! $ALL && error "--domain or --all is required"
-$ALL && DOMAINS+=('-')
 
 check_file "$KEY"
 check_file "$CERT"
@@ -90,24 +179,38 @@ for DOMAIN in ${DOMAINS[@]}; do
   SERVER_NAME="server_name $DOMAIN;"
   [ "*.${DOMAIN:2}" == "$DOMAIN" ] && WILDCARD_SERVER_NAME="server_name ${DOMAIN:2};"
 
-  cat <<EOF_SSL >> /tmp/nginx.ssl.conf
+  cat <<EOF >> /tmp/nginx.ssl.conf
 server {
   listen 80;
   $SERVER_NAME
   $WILDCARD_SERVER_NAME
   return 301 https://\$host\$request_uri;
 }
-EOF_SSL
+EOF
 done
 
-cat <<EOF_SSL >> /tmp/nginx.ssl.conf
+cat <<EOF >> /tmp/nginx.ssl.conf
 # default config (server_name _; makes this 'base' config)
+
 server {
-  listen 443 default ssl;  
+  listen 80;
   server_name _;
 
-  ssl_certificate_key $(realpath "$KEY");
-  ssl_certificate $(realpath "$CERT");
+  location /.well-known/acme-challenge {
+    root /var/www/letsencrypt;
+  }
+}
+
+server {
+  listen 443 default ssl;
+  server_name _;
+
+  location /.well-known/acme-challenge {
+    root /var/www/letsencrypt;
+  }
+
+  ssl_certificate_key $(realpath -s "$KEY");
+  ssl_certificate $(realpath -s "$CERT");
 
   # These this next block of settings came directly from the SSLMate recommend nginx configuration
   # Recommended security settings from https://wiki.mozilla.org/Security/Server_Side_TLS
@@ -126,10 +229,10 @@ server {
   # openssl dhparam -outform pem -out dhparam2048.pem 2048
   $SSL_DHPARAM
 }
-EOF_SSL
+EOF
 
-[ ! -O /etc/nginx/conf.d ] && SUDO_MAYBE=sudo
 $SUDO_MAYBE mv /tmp/nginx.ssl.conf /etc/nginx/conf.d/ssl.conf
 $SUDO_MAYBE nginx -s reload
+$LETSENCRYPT && $SUDO_MAYBE renew_cert_timer
 
 echo "Wrote nginx SSL config to /etc/nginx/conf.d/ssl.conf"
